9c0651a28f5f3bd3ec9aacfdcf3b698584330edb
sadiabup <sadiasultanamim01863@gmail.com>
2025-01-17 03:24:51 -0600

    Add files via upload

---
+++ dataFile/testData.ts
@@ -0,0 +1,23 @@
+export const testData = {
+    url: "https://test460.nop-station.com/en/",
+    email: "sadia111@gmail.com",
+    password: "12345678",
+    billingDetails: {
+        firstName: "Sadia",
+        lastName: "Sultana",
+        email: "sadia111@gmail.com",
+        company: "BS",
+        country: "Bangladesh",
+        state: "ঢাকা",
+        city: "Dhaka",
+        address1: "Cantonment",
+        address2: "",
+        zip: "123",
+        phone: "019299999999",
+        fax: "",
+    },
+    product: {
+        name: "Vintage Style Engagement Ring",
+        quantity: 3,
+    },
+};
